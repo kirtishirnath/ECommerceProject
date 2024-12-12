9b28bb34d2545da2027adb50600ebf53cfbe4c25
kirtishirnath <kirtishirnath@gmail.com>
2024-12-12 16:38:03 +0530

    Added product module.

--- ecommerce/product/src/test/java/com/product/ProductApplicationTests.java
+++ ecommerce/product/src/test/java/com/product/ProductApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecommerce;
+package com.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class ProductApplicationTests {
 
 	@Test
 	void contextLoads() {
